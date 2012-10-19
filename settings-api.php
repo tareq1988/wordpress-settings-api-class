ad427c03753a65f0424f18ea484f418a806dc9a1
Rinat Khaziev <rinat.khaziev@gmail.com>
2012-10-19 12:13:59 -0500

    It's better not to use singleton at all, it might potentially break multiple plugins at once( since they might share one instance and just override each others properties )

--- settings-api.php
+++ settings-api.php
@@ -20,7 +20,7 @@ class WeDevs_Settings_API_Test {
     private $settings_api;
 
     function __construct() {
-        $this->settings_api = WeDevs_Settings_API::getInstance();
+        $this->settings_api = new WeDevs_Settings_API;
 
         add_action( 'admin_init', array($this, 'admin_init') );
         add_action( 'admin_menu', array($this, 'admin_menu') );
