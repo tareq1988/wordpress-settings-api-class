6ed507c0daf7da19d8ca34ebb6a93b9895123c6b
Rinat Khaziev <rinat.khaziev@gmail.com>
2013-03-29 13:56:38 -0500

    Moving js from php class to a separate file

--- class.settings-api.php
+++ class.settings-api.php
@@ -32,7 +32,8 @@ if ( !class_exists( 'WeDevs_Settings_API' ) ):
     private static $_instance;
 
     public function __construct() {
-        add_action('admin_enqueue_scripts', array(&$this, 'admin_enqueue_scripts'));
+        add_action('admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+        add_action( 'admin_head', array( $this, 'admin_head' ) );
 
     }
 
@@ -40,13 +41,21 @@ if ( !class_exists( 'WeDevs_Settings_API' ) ):
     * Enqueue scripts and styles
     */
     function admin_enqueue_scripts() {
-        wp_enqueue_style('thickbox');
-
         wp_enqueue_script('jquery');
         wp_enqueue_script('media-upload');
         wp_enqueue_script('thickbox');
     }
-        
+    
+    /**
+     * Print the JS in <head> instead of inlining it in body
+     * 
+     */
+    function admin_head() {
+        ob_start();
+        require_once( __DIR__ . '/inc/js/wsa.js' );
+        $output = '<script>' . ob_get_clean() . '</script>';
+        echo $output;
+    }    
     /**
      * Set settings sections
      *
@@ -389,61 +398,6 @@ if ( !class_exists( 'WeDevs_Settings_API' ) ):
             </div>
         </div>
         <?php
-        $this->script();
     }
-
-    /**
-     * Tabbable JavaScript codes
-     *
-     * This code uses localstorage for displaying active tabs
-     */
-    function script() {
-        ?>
-        <script>
-            jQuery(document).ready(function($) {
-                // Switches option sections
-                $('.group').hide();
-                var activetab = '';
-                if (typeof(localStorage) != 'undefined' ) {
-                    activetab = localStorage.getItem("activetab");
-                }
-                if (activetab != '' && $(activetab).length ) {
-                    $(activetab).fadeIn();
-                } else {
-                    $('.group:first').fadeIn();
-                }
-                $('.group .collapsed').each(function(){
-                    $(this).find('input:checked').parent().parent().parent().nextAll().each(
-                    function(){
-                        if ($(this).hasClass('last')) {
-                            $(this).removeClass('hidden');
-                            return false;
-                        }
-                        $(this).filter('.hidden').removeClass('hidden');
-                    });
-                });
-
-                if (activetab != '' && $(activetab + '-tab').length ) {
-                    $(activetab + '-tab').addClass('nav-tab-active');
-                }
-                else {
-                    $('.nav-tab-wrapper a:first').addClass('nav-tab-active');
-                }
-                $('.nav-tab-wrapper a').click(function(evt) {
-                    $('.nav-tab-wrapper a').removeClass('nav-tab-active');
-                    $(this).addClass('nav-tab-active').blur();
-                    var clicked_group = $(this).attr('href');
-                    if (typeof(localStorage) != 'undefined' ) {
-                        localStorage.setItem("activetab", $(this).attr('href'));
-                    }
-                    $('.group').hide();
-                    $(clicked_group).fadeIn();
-                    evt.preventDefault();
-                });
-            });
-        </script>
-        <?php
-    }
-
 }
 endif;
---
+++ inc/js/wsa.js
@@ -0,0 +1,47 @@
+/**
+ * Tabbable JavaScript codes
+ *
+ * This code uses localstorage for displaying active tabs
+ */
+
+jQuery(document).ready(function($) {
+	// Switches option sections
+	$('.group').hide();
+	var activetab = '';
+	if (typeof(localStorage) != 'undefined' ) {
+		activetab = localStorage.getItem("activetab");
+	}
+	if (activetab != '' && $(activetab).length ) {
+		$(activetab).fadeIn();
+	} else {
+		$('.group:first').fadeIn();
+	}
+	$('.group .collapsed').each(function(){
+		$(this).find('input:checked').parent().parent().parent().nextAll().each(
+		function(){
+			if ($(this).hasClass('last')) {
+				$(this).removeClass('hidden');
+				return false;
+			}
+			$(this).filter('.hidden').removeClass('hidden');
+		});
+	});
+
+	if (activetab != '' && $(activetab + '-tab').length ) {
+		$(activetab + '-tab').addClass('nav-tab-active');
+	}
+	else {
+		$('.nav-tab-wrapper a:first').addClass('nav-tab-active');
+	}
+	$('.nav-tab-wrapper a').click(function(evt) {
+		$('.nav-tab-wrapper a').removeClass('nav-tab-active');
+		$(this).addClass('nav-tab-active').blur();
+		var clicked_group = $(this).attr('href');
+		if (typeof(localStorage) != 'undefined' ) {
+			localStorage.setItem("activetab", $(this).attr('href'));
+		}
+		$('.group').hide();
+		$(clicked_group).fadeIn();
+		evt.preventDefault();
+	});
+});
