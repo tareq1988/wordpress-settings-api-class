98ca4ba3a1de94bbb23d48e1345927bd16ba3f32
Tareq Hasan <tareq1988@gmail.com>
2015-04-23 19:38:05 +0600

    New plugin file added in-favour of separate example file

---
+++ plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: WordPress Settings API
+ * Plugin URI: http://tareq.wedevs.com/2012/06/wordpress-settings-api-php-class/
+ * Description: WordPress Settings API testing
+ * Author: Tareq Hasan
+ * Author URI: http://tareq.weDevs.com
+ * Version: 0.1
+ */
+
+require_once dirname( __FILE__ ) . '/src/class.settings-api.php';
+require_once dirname( __FILE__ ) . '/example/oop-example.php';
+
+new WeDevs_Settings_API_Test();
