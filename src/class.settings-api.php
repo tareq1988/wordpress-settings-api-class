52f96ac3b0202464ea30dba93d915c01c94acd73
Tareq Hasan <tareq1988@gmail.com>
2015-05-19 19:14:49 +0600

    Hide content during page load

--- src/class.settings-api.php
+++ src/class.settings-api.php
@@ -474,7 +474,7 @@ class WeDevs_Settings_API {
         ?>
         <div class="metabox-holder">
 			<?php foreach ( $this->settings_sections as $form ) { ?>
-				<div id="<?php echo $form['id']; ?>" class="group">
+				<div id="<?php echo $form['id']; ?>" class="group" style="display: none;">
 					<form method="post" action="options.php">
 						<?php
 						do_action( 'wsa_form_top_' . $form['id'], $form );
