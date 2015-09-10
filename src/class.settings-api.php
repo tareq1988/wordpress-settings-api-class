c577a236058e205ccc94c47a9e81257eefcddb04
Corey Maass <corey@gelform.com>
2015-09-10 14:34:28 -0500

    when saving an empty multicheck, the $options passed to sanitize_options() is null, which causes an error. I've added a check for is_null before trying to sanitize $options.

--- src/class.settings-api.php
+++ src/class.settings-api.php
@@ -394,15 +394,18 @@ class WeDevs_Settings_API {
      * Sanitize callback for Settings API
      */
     function sanitize_options( $options ) {
-        foreach( $options as $option_slug => $option_value ) {
-            $sanitize_callback = $this->get_sanitize_callback( $option_slug );
+        if ( !is_null($options) ) {
+            foreach( $options as $option_slug => $option_value ) {
+                $sanitize_callback = $this->get_sanitize_callback( $option_slug );
 
-            // If callback is set, call it
-            if ( $sanitize_callback ) {
-                $options[ $option_slug ] = call_user_func( $sanitize_callback, $option_value );
-                continue;
+                // If callback is set, call it
+                if ( $sanitize_callback ) {
+                    $options[ $option_slug ] = call_user_func( $sanitize_callback, $option_value );
+                    continue;
+                }
             }
         }
+
 
         return $options;
     }
