898716555ad80a097ede5153f674cd9d37632e8b
Roi Dayan <roi.dayan@gmail.com>
2016-09-08 08:13:39 +0300

    Dont show submit button if there are no input fields (#49)
    * Refactor tabs to spaces
    
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>
    
    * Dont show submit button if there are no input fields
    
    Can be used to add a section with information only.
    Like a help section.
    
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- src/class.settings-api.php
+++ src/class.settings-api.php
@@ -473,21 +473,23 @@ class WeDevs_Settings_API {
     function show_forms() {
         ?>
         <div class="metabox-holder">
-			<?php foreach ( $this->settings_sections as $form ) { ?>
-				<div id="<?php echo $form['id']; ?>" class="group" style="display: none;">
-					<form method="post" action="options.php">
-						<?php
-						do_action( 'wsa_form_top_' . $form['id'], $form );
-						settings_fields( $form['id'] );
-						do_settings_sections( $form['id'] );
-						do_action( 'wsa_form_bottom_' . $form['id'], $form );
-						?>
-						<div style="padding-left: 10px">
-							<?php submit_button(); ?>
-						</div>
-					</form>
-				</div>
-			<?php } ?>
+            <?php foreach ( $this->settings_sections as $form ) { ?>
+                <div id="<?php echo $form['id']; ?>" class="group" style="display: none;">
+                    <form method="post" action="options.php">
+                        <?php
+                        do_action( 'wsa_form_top_' . $form['id'], $form );
+                        settings_fields( $form['id'] );
+                        do_settings_sections( $form['id'] );
+                        do_action( 'wsa_form_bottom_' . $form['id'], $form );
+                        if ( isset( $this->settings_fields[ $form['id'] ] ) ):
+                        ?>
+                        <div style="padding-left: 10px">
+                            <?php submit_button(); ?>
+                        </div>
+                        <?php endif; ?>
+                    </form>
+                </div>
+            <?php } ?>
         </div>
         <?php
         $this->script();
