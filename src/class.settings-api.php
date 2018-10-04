d66a9f7eceb8eaec2e2bf16f969b7266c301a30a
Pablo dos Santos Gonçalves Pacheco <pablo.sg.pacheco@gmail.com>
2018-10-04 01:24:07 -0300

    Create a subsection field
    Now it will be possible to create a subsection field improving the organization
    
    ```
    array(
    						'name'        => 'subsection',
    						'label'       => __( 'My subsection', 'remove-special-characters-from-permalinks' ),
    						'desc'        => __( 'Now we can have a subsection description too ;)', 'remove-special-characters-from-permalinks' ),
    						'type'        => 'subsection'
    					)
    ```

--- src/class.settings-api.php
+++ src/class.settings-api.php
@@ -521,7 +521,7 @@ class WeDevs_Settings_API {
     }
 
     /**
-     * Show the section settings forms
+     * Show the section settings forms using a custom do_settings_sections()
      *
      * This function displays every sections in a different form
      */
@@ -534,7 +534,7 @@ class WeDevs_Settings_API {
                         <?php
                         do_action( 'wsa_form_top_' . $form['id'], $form );
                         settings_fields( $form['id'] );
-                        do_settings_sections( $form['id'] );
+                        $this->do_settings_sections( $form['id'] );
                         do_action( 'wsa_form_bottom_' . $form['id'], $form );
                         if ( isset( $this->settings_fields[ $form['id'] ] ) ):
                         ?>
@@ -652,6 +652,104 @@ class WeDevs_Settings_API {
         <?php
         endif;
     }
+	
+	/**
+	 * Remove the h2 section title and call the custom do_settings_fields
+	 *
+	 * @see \do_settings_sections() from template.php
+	 * @param $page
+	 */
+	function do_settings_sections( $page ) {
+		global $wp_settings_sections, $wp_settings_fields;
+
+		if ( ! isset( $wp_settings_sections[ $page ] ) ) {
+			return;
+		}
+
+		foreach ( (array) $wp_settings_sections[ $page ] as $section ) {
+			if ( $section['callback'] ) {
+				call_user_func( $section['callback'], $section );
+			}
+
+			if ( ! isset( $wp_settings_fields ) || ! isset( $wp_settings_fields[ $page ] ) || ! isset( $wp_settings_fields[ $page ][ $section['id'] ] ) ) {
+				continue;
+			}
+			echo '<table class="form-table">';
+			$this->do_settings_fields( $page, $section['id'] );
+			echo '</table>';
+		}
+	}
+	
+	/**
+	 * If there is a type == subsection, just call the call_user_func() instead of creating the tr
+	 *
+	 * @see \do_settings_fields() from template.php
+	 * @param $page
+	 * @param $section
+	 */
+	function do_settings_fields( $page, $section ) {
+		global $wp_settings_fields;
+
+		if ( ! isset( $wp_settings_fields[ $page ][ $section ] ) ) {
+			return;
+		}
+
+		foreach ( (array) $wp_settings_fields[ $page ][ $section ] as $field ) {
+			$class = '';
+
+			if ( $field['args']['type'] != 'subsection' ) {
+				if ( ! empty( $field['args']['class'] ) ) {
+					$class = ' class="' . esc_attr( $field['args']['class'] ) . '"';
+				}
+
+				echo "<tr{$class}>";
+
+				if ( ! empty( $field['args']['label_for'] ) ) {
+					echo '<th scope="row"><label for="' . esc_attr( $field['args']['label_for'] ) . '">' . $field['title'] . '</label></th>';
+				} else {
+					echo '<th scope="row">' . $field['title'] . '</th>';
+				}
+
+				echo '<td>';
+				call_user_func( $field['callback'], $field['args'] );
+				echo '</td>';
+				echo '</tr>';
+			} else {
+				call_user_func( $field['callback'], $field['args'] );
+			}
+		}
+	}
+	
+	/**
+	 * Displays the html for a subsection field
+	 *
+	 * @param array   $args settings field args
+	 * @return string
+	 */
+	public function callback_subsection( $args ) {
+		if ( ! isset( $args['name'] ) ) {
+			return;
+		}
+		$subection = $args['name'];
+		if ( empty( $subection ) ) {
+			return;
+		}
+		if ( isset( $args['desc'] ) ) {
+			$desc = $args['desc'];
+		}
+
+		?>
+	    </table>
+	    <h2><?php echo esc_html( $subection ) ?></h2>
+		<?php if ( ! empty( $desc ) ): ?>
+		<p><?php echo esc_html( $desc ); ?></p>
+		<?php endif; ?>
+	    <table class="form-table">
+		<tbody>
+		<?php
+	}
+
+	
 
 }
 
